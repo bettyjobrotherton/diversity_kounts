978dff01afa760adef339cea7c8ed5b48b49946e
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2016-11-29 20:53:23 -0500

    Isn't working for me. Going to try to start a new branch from the master and go from there

--- server/routers/user.router.js
+++ server/routers/user.router.js
@@ -3,23 +3,35 @@ var router = express.Router();
 
 var User = require('../models/user.model.js');
 
-router.get('/users', function(req, res){
-  User.find({}, function(err, users){
+// router.get('/users', function(req, res){
+//   User.find({}, function(err, users){
+//     if(err){
+//       res.status(500).json({
+//         msg: err   //change to generic error message
+//       });
+//     } else{
+//       res.status(200).json({
+//         posts: posts
+//       });
+//     }
+//   });
+// });
+
+router.get();
+
+router.post('users/signup', function(req, res){
+  User.save(function(err){
     if(err){
       res.status(500).json({
-        msg: err   //change to generic error message
+        msg: err
       });
-    } else{
-      res.status(200).json({
-        posts: posts
+    } else {
+      res.status(201).json({
+        msg: 'Account successfully created'
       });
     }
   });
 });
-
-router.get();
-
-router.post();
 
 router.put();
 
