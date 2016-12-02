11576ddf282f51954e7761fc51ec5e859e8b83ac
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2016-12-01 22:09:01 -0500

    Created salt, hash, and jwt in user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,5 +1,9 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+
+var signature = process.env.SIGNATURE || require('../../config.js').signature;
 
 var userSchema = new Schema ({
   firstName: {
@@ -40,6 +44,27 @@ var userSchema = new Schema ({
     type: String
   }
 });
+
+userSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+userSchema.methods.validPassword = function(password){
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+  return this.hash === hash;
+};
+
+userSchema.methods.generateJwt = function(){
+  var expiration = new Date();
+  expiration.setDate(expiration.getDate() + 1 );
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    exp: parseInt(expiration.getTime() /1000)
+  }, signature);
+};
 
 var User = mongoose.model('User', userSchema);
 
