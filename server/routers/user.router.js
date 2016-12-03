75a80c8a0c4fd00535d4759eb874796bfa682e3a
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2016-12-02 19:51:08 -0500

    Added login route to user router

--- server/routers/user.router.js
+++ server/routers/user.router.js
@@ -42,7 +42,33 @@ router.post('/users/signup', function(req, res){
   });
 });
 
-router.post('/users/login', function(req, res){});
+router.post('/users/login', function(req, res){
+  if(!req.body.email || !req.body.password){
+    return res.status(401).json({
+      msg: 'The email or password you provided is incorrect.'
+    });
+  }
+  User.findOne({email: req.body.email}, function(err, user){
+    if(err){
+      return res.status(500).json({
+        msg:err
+      });
+    }
+    if(!user){
+      return res.status(401).json({
+        msg: 'The email or password you provided is incorrect.'
+      });
+    }
+    if(!user.validPassword(req.body.password)){
+      return res.status(401).json({
+        msg: 'The email or password you provided is incorrect.'
+      });
+    }
+    return res. status(200).json({
+      token: user.generateJwt()
+    });
+  });
+});
 
 router.put('/users/profile/:userID', function(req, res){});
 
