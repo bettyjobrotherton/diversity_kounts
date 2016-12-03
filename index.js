f33a7e4edd1e76ffb942f846ee4980ce84bbae69
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2016-12-02 20:36:11 -0500

    Saving changes to pull from remote

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var express = require('express');
 var server = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var userRouter = require('./server/routers/user.router.js');
 
 var port = process.env.PORT || 8080;
-var mongoURI; //need link to MongoDB;
+var mongoURI = process.env.MONGOURI || require('./config.js').databaseURI;
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
@@ -16,6 +17,8 @@ server.use(express.static(__dirname + '/public'));
 server.get('/', function(req, res){
   res.sendFile('public/html/index.html', {root: __dirname});
 });
+
+server.use(userRouter);
 
 server.listen(port, function(){
   console.log('Now listening on port...', port);
