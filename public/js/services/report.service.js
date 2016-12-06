c464e3869d78510e109d7efa71122e7dd76044a8
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2016-12-05 20:40:24 -0500

    Started writing report service

---
+++ public/js/services/report.service.js
@@ -0,0 +1,74 @@
+(function() {
+
+  angular.module('diversity_app')
+         .factory('ReportService', ReportService);
+
+  ReportService.$inject = ['$http'];
+
+  function ReportService($http){
+    var reports = [];
+    var userReports = [];
+    var selectedReport;
+    var baseURL = '/reports/';
+
+    init();
+
+    return {
+      getAll: getAll,
+      getAllByUser: getAllByUser,
+      getOne: getOne,
+      getSelected: getSelected,
+      create: create,
+      update: update,
+      delete: deleteOne
+    };
+
+    function init(){
+      $http.get(baseURL)
+           .then(function(res){
+             reports = res.data.reports;
+           })
+           .catch(function(err){
+             console.log(err);
+           });
+    }
+
+    function getAll(){
+      return reports;
+    }
+
+    function getAllByUser(userID){
+      $http.get(baseURL + 'byuser/' + userID)
+           .then(function(res){
+             userReports = res.data.reports;
+             return userReports;
+           })
+           .catch(function(err){
+             console.log(err);
+           });
+    }
+
+    function getOne(id){
+      $http.get(baseURL + id)
+           .then(function(res){
+             selectedReport = res.data.report[0];
+             return selectedReport;
+           })
+           .catch(function(err){});
+    }
+
+    function getSelected(){
+      return selectedReport;
+    }
+
+    function create(newReport){
+      $http.post(baseURL)
+    }
+
+    function update(){}
+
+    function deleteOne(){}
+
+  }
+
+}());
