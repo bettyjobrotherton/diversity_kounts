8e5192d4780c132276386497dfab22742faf19bf
johned <johned.matt@gmail.com>
2016-12-07 05:14:04 -0500

    initial commit - added frontend, incorporating forms to styled frontend

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 var express = require('express');
 var server = express();
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+// var mongoose = require('mongoose');
+// var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 8080;
-var mongoURI = process.env.MONGOURI || require('./config.js').databaseURI;
+// var mongoURI = process.env.MONGOURI || require('./config.js').databaseURI;
 
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({extended: true}));
+// server.use(bodyParser.json());
+// server.use(bodyParser.urlencoded({extended: true}));
 
-mongoose.connect(mongoURI);
+// mongoose.connect(mongoURI);
 
 server.use(express.static(__dirname + '/public'));
 
-server.get('/', function(req, res){
-  res.sendFile('public/html/index.html', {root: __dirname});
+server.get("/", function(request, response){
+  response.sendFile("public/html/index.html", {root: __dirname});
+});
+
+server.get("/login", function(request, response){
+  response.sendFile("public/html/login.html", { root: __dirname });
+});
+
+server.get("/user", function(request, response){
+  response.sendFile('public/html/user.html', { root: __dirname });
 });
 
 server.listen(port, function(){
-  console.log('Now listening on port...', port);
+  console.log("Now listening on port...", port);
 });
